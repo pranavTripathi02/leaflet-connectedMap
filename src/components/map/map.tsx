689d086bde83b643158be7a07080486e6c2f54af
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-11 18:37:30 +0530

    pin clustering added

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet.markercluster/dist/leaflet.markercluster";
 import L from "leaflet";
-import MarkerIcon from "./marker";
-import { renderToString } from "react-dom/server";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import useMapContext from "@/hooks/useMapContext";
 import { useEffect, useState } from "react";
 import Loading from "../loading";
@@ -12,10 +13,31 @@ import Loading from "../loading";
 function Map() {
   const { userList } = useMapContext();
   const [mount, setMount] = useState(false);
-  useEffect(() => setMount(true), []);
+  const [map, setMap] = useState<L.Map | null>(null);
+  console.log();
+  useEffect(() => {
+    setMount(true);
+  }, []);
   if (!mount) {
     return <Loading />;
   }
+  const mapOpts = () => {
+    if (!map) return;
+    map?.zoomControl?.setPosition("bottomright");
+    const markers = L.markerClusterGroup({
+      iconCreateFunction: function (cluster) {
+        return L.divIcon({ html: "<b>" + cluster.getChildCount() + "</b>" });
+      },
+    });
+    userList.forEach(
+      (user) =>
+        user.location.lat &&
+        user.location.lng &&
+        markers.addLayer(L.marker([user.location.lat, user.location.lng])),
+    );
+    map.addLayer(markers);
+  };
+  mapOpts();
   return (
     <MapContainer
       className="z-0 h-full w-full"
@@ -28,26 +50,27 @@ function Map() {
       dragging
       bounceAtZoomLimits
       worldCopyJump
-      zoomControl={false}
+      ref={setMap}
+      whenReady={mapOpts}
     >
       <TileLayer
         attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {userList &&
-        userList.map((user) => {
-          if (!user.location.lat || !user.location.lng) return null;
-          return (
-            <Marker
-              key={user.id}
-              position={[user.location.lat, user.location.lng]}
-              icon={L.divIcon({
-                className: "",
-                html: renderToString(<MarkerIcon />),
-              })}
-            />
-          );
-        })}
+      {/* {userList && */}
+      {/*   userList.map((user) => { */}
+      {/*     if (!user.location.lat || !user.location.lng) return null; */}
+      {/*     return ( */}
+      {/*       <Marker */}
+      {/*         key={user.id} */}
+      {/*         position={[user.location.lat, user.location.lng]} */}
+      {/*         icon={L.divIcon({ */}
+      {/*           className: "", */}
+      {/*           html: renderToString(<MarkerIcon />), */}
+      {/*         })} */}
+      {/*       /> */}
+      {/*     ); */}
+      {/*   })} */}
     </MapContainer>
   );
 }
