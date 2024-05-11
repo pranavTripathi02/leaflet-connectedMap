043b223d1b211b10b25eef67e36ae6f1ac7563ab
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-11 06:51:44 +0530

    added markers for all users

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,7 +1,10 @@
 "use client";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import useMapContext from "@/hooks/useMapContext";
+import L from "leaflet";
+import MarkerIcon from "./marker";
+import { renderToString } from "react-dom/server";
 
 function Mapview() {
   const { userList } = useMapContext();
@@ -17,6 +20,19 @@ function Mapview() {
         attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {userList.map((user) => {
+        if (!user.location.lat || !user.location.lng) return null;
+        return (
+          <Marker
+            key={user.id}
+            position={[user.location.lat, user.location.lng]}
+            icon={L.divIcon({
+              className: "",
+              html: renderToString(<MarkerIcon />),
+            })}
+          />
+        );
+      })}
     </MapContainer>
   );
 }
