17bcce7cab88d159c4ad28febf7a59c4fdd082ab
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-11 15:21:34 +0530

    topbar ui changes

--- src/components/map/mapTopBar.tsx
+++ src/components/map/mapTopBar.tsx
@@ -16,15 +16,11 @@ function MapTopBar() {
     [debouncedSearchText],
   );
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchbarText(e.target.value);
-  };
-
   return (
     <div className="back absolute left-0 right-0 top-4 z-20 h-fit w-full rounded bg-black/5 px-4 py-2 md:top-10 md:bg-transparent">
       <div className="flex items-start justify-between">
         <div className="group peer flex flex-col gap-4">
-          <div className="group flex w-fit items-center rounded focus-within:bg-white focus-within:outline-blue-700 md:bg-white">
+          <div className="group flex w-fit items-center rounded outline-blue-700 focus-within:bg-white focus-within:outline md:bg-white">
             {/* Search Icon */}
             <button
               onClick={() => {
@@ -51,43 +47,41 @@ function MapTopBar() {
               ref={searchInputRef}
               className="w-0 bg-transparent outline-none duration-200 focus:w-40 focus:px-4 focus:py-2 md:w-96 md:px-4 md:py-2 md:focus:w-96"
               value={searchbarText}
-              onChange={handleSearch}
+              onChange={(e) => setSearchbarText(e.target.value)}
               placeholder="Search Users"
             />
           </div>
-          <div className="hidden max-h-[512px] flex-col gap-2 overflow-y-scroll rounded bg-white py-4 group-focus-within:flex group-focus-within:empty:hidden">
-            {searchbarText.length > 2
-              ? userList.map((user) => {
-                  return (
-                    <div
-                      key={user.id}
-                      className="flex items-center gap-4 rounded border-b border-black/5 px-4 py-4 last:border-0"
-                    >
-                      <div className="h-[64px] w-[64px] rounded-full border-2">
-                        {user.photo && (
-                          <Image
-                            src={user.photo}
-                            height={64}
-                            width={64}
-                            alt={user.fullName}
-                            className="break-after-all text-center text-sm"
-                          />
-                        )}
-                      </div>
-                      <div className="flex flex-col">
-                        {/* name */}
-                        <p className="font-bold">{user.fullName}</p>
-                        {/* location */}
-                        <p className="break-normal">
-                          {user.location.city && user.location.city + ", "}
-                          {user.location.state && user.location.state + ", "}
-                          {user.location.country && user.location.country}
-                        </p>
-                      </div>
-                    </div>
-                  );
-                })
-              : null}
+          <div className="hidden max-h-[384px] min-w-fit max-w-[400px] flex-col gap-2 overflow-y-scroll rounded py-4 group-focus-within:flex md:max-h-[512px]">
+            {userList.map((user) => {
+              return (
+                <div
+                  key={user.id}
+                  className="flex w-full items-center gap-4 rounded border-b border-black/5 bg-white px-4 py-4 last:border-0"
+                >
+                  <div className="h-[64px] w-[64px] rounded-full border-2">
+                    {user.photo && (
+                      <Image
+                        src={user.photo}
+                        height={64}
+                        width={64}
+                        alt={user.fullName}
+                        className="break-after-all text-center text-sm"
+                      />
+                    )}
+                  </div>
+                  <div className="flex flex-col">
+                    {/* name */}
+                    <p className="font-bold">{user.fullName}</p>
+                    {/* location */}
+                    <p className="break-normal">
+                      {user.location.city && user.location.city + ", "}
+                      {user.location.state && user.location.state + ", "}
+                      {user.location.country && user.location.country}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="flex gap-4 py-2 peer-focus-within:hidden">
