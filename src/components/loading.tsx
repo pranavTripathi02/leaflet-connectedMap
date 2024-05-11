bc4331df7009ca82d33f71aad1a5d28343b688aa
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-11 18:37:54 +0530

    loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,22 @@
+function Loading() {
+  return (
+    <div className=" h-full w-full">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="80"
+        height="80"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-loader-circle m-auto h-full animate-spin opacity-50"
+      >
+        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+      </svg>
+    </div>
+  );
+}
+
+export default Loading;
